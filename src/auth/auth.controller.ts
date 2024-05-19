47614fc09989a9819fba5df9f16eb0ff2065d59d
SorrawitRUNGRUJEE <sorrawit_r@outlook.com>
2024-05-19 16:08:40 +0700

    feat:connect service to controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,21 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { authPayloadDto } from './dto/auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+    constructor(private readonly authService: AuthService){
+        
+    }
     @Post('signUp')
     signUp(){
-        return "this is sign up route"
+        return this.authService.signUp()
     }
 
 
     @Post('signIn')
     signUn(){
-        return "this is sign in route"
+        return this.authService.signIn()
     }
     // login(@Body() authpayload: authPayloadDto){
 
@@ -19,6 +23,6 @@ export class AuthController {
 
     @Get('signOut')
     signOut(){
-        return "this is sign out route"
+        return this.authService.signOut()
         }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,16 @@ import { authPayloadDto } from './dto/auth.dto';
 import { Prisma } from '@prisma/client';
 @Injectable()
 export class AuthService {
-    validateUser({username,password}:authPayloadDto){
-        // const existUser = 
+    constructor(){}
+    async signUp(){
+        return {message:"sign up complete"}
+    }
+
+    async signIn(){
+        return {message:"sign in complete"}
+    }
+
+    async signOut(){
+        return {message:"sign out complete"}
     }
 }
